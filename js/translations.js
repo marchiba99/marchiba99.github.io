2e2354a7bb24d31f78d8e7935bcc8c8537e0bb78
Marc Hidalgo <marc.hidalgo@csuc.cat>
2023-05-02 14:41:42 +0200

    change language button

--- js/translations.js
+++ js/translations.js
@@ -1,13 +1,32 @@
-function aplicarTraducciones() {
-  // Obtener el valor del atributo "lang" del elemento "html"
-  const lang = document.documentElement.lang;
+function crearSelectorDeIdioma() {
+  // Crear el elemento "select" con las opciones de idioma
+  const select = document.createElement('select');
+  const optionEs = document.createElement('option');
+  optionEs.value = 'es';
+  optionEs.textContent = 'Español';
+  const optionEn = document.createElement('option');
+  optionEn.value = 'en';
+  optionEn.textContent = 'English';
+  const optionCa = document.createElement('option');
+  optionCa.value = 'ca';
+  optionCa.textContent = 'Català';
+  select.appendChild(optionEs);
+  select.appendChild(optionEn);
+  select.appendChild(optionCa);
 
-  // Cargar el archivo JSON correspondiente al idioma
-  fetch(`languages/${lang}.json`)
-    .then(response => response.json())
-    .then(traducciones => {
-      // Asignar las traducciones a los elementos del HTML
-      document.getElementById('hola').textContent = traducciones['key1'];
-      // Aquí puedes seguir asignando las traducciones a otros elementos del HTML
-    });
+  // Agregar un evento "change" al elemento "select" para cambiar el idioma
+  select.addEventListener('change', (event) => {
+    const lang = event.target.value;
+    // Cargar el archivo JSON correspondiente al idioma
+    fetch(`languages/${lang}.json`)
+      .then(response => response.json())
+      .then(traducciones => {
+        // Asignar las traducciones a los elementos del HTML
+        document.getElementById('hola').textContent = traducciones['key1'];
+        // Aquí puedes seguir asignando las traducciones a otros elementos del HTML
+      });
+  });
+
+  // Agregar el elemento "select" al elemento "div" con el identificador "language-switcher"
+  document.getElementById('language-switcher').appendChild(select);
 }
